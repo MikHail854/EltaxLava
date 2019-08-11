949dc5e738557a43b1060fbd6cb4dc5d56e6d0a7
mikhail chigurov <vip.moneta95@mail.ru>
2019-08-11 23:54:29 +0700

    Lab 1 finished

--- lab1/Main.java
+++ lab1/Main.java
@@ -1,24 +1,18 @@
 package ru.eltex.app.lab1;
 
-import ru.eltex.app.lab2.Credentails;
-import ru.eltex.app.lab2.Orders;
-import ru.eltex.app.lab2.ShoppingCart;
-
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args){
         /**
          *  программа работает от аргумента, в зависимости от вида представления(чай или кофе)
          */
 
         Scanner scanner = new Scanner(System.in);
-        Credentails user = new Credentails();
-        System.out.println("\nМеню:\n1.Телефон\n2.Смартфон\n3.Планшет\n");
-        int choice = scanner.nextInt();
-        if (choice == 1) {
-            System.out.println("Введите желаемое кол-во товара: ");
+
+        if (args[0].equalsIgnoreCase("телефон")){
+            System.out.println("Введите желаемое кол-во товара: " + args[0].toLowerCase());
             int Object1 = scanner.nextInt();
             Phone[] phone = new Phone[Object1];
             for (int i = 0; i < Object1; i++) {
@@ -26,24 +20,17 @@ public class Main {
                 phone[i].create();
                 phone[i].read();
             }
-            ShoppingCart cart = new ShoppingCart();
-            for (int i = 0; i < Object1; i++) {
-                cart.add(phone[i]);
-            }
+
             System.out.println("\nКол-во товара: " + Products.CounterObject);
-            System.out.println("\n1. Оформить заказ\n2. Выход");
-            int choiсe2 = scanner.nextInt();
-            if (choiсe2 == 1) {
-                Orders orders = new Orders();
-                orders.offer(cart, user);
-                Thread.sleep(10);
-                orders.checkTime();
-                orders.show();
-                System.out.println("Проверка: " + cart.isExistsUUID(phone[0].getUUID()));
-            }
-        }
-        if (choice == 2) {
-            System.out.println("Введите желаемое кол-во товара: ");
+            /**
+             * Пример работы функции update
+             */
+            System.out.println("Ручной ввод");
+            phone[0].delete();
+            phone[0].update();
+            phone[0].read();
+        } else if (args[0].equalsIgnoreCase("смартфон")){
+            System.out.println("Введите желаемое кол-во товара: " + args[0].toLowerCase());
             int Object2 = scanner.nextInt();
             Smartphone[] smartphone = new Smartphone[Object2];
             for (int i = 0; i < Object2; i++) {
@@ -51,24 +38,16 @@ public class Main {
                 smartphone[i].create();
                 smartphone[i].read();
             }
-            ShoppingCart cart = new ShoppingCart();
-            for (int i = 0; i < Object2; i++) {
-                cart.add(smartphone[i]);
-            }
             System.out.println("\nКол-во объектов: " + Products.CounterObject);
-            System.out.println("\n1.Оформить заказ\n2.Выход");
-            int choice2 = scanner.nextInt();
-            if (choice2 == 1) {
-                Orders orders = new Orders();
-                orders.offer(cart, user);
-                Thread.sleep(10);
-                orders.checkTime();
-                orders.show();
-                System.out.println("Проверка: " + cart.isExistsUUID(smartphone[0].getUUID()));
-            }
-        }
-        if (choice == 3) {
-            System.out.println("Введите желаемое кол-во товара: ");
+            /**
+             * Пример работы функции update
+             */
+            System.out.println("Ручной ввод");
+            smartphone[0].delete();
+            smartphone[0].create();
+            smartphone[0].read();
+        }else if (args[0].equalsIgnoreCase("планшет")) {
+            System.out.println("Введите желаемое кол-во товара: " + args[0].toLowerCase());
             int Object3 = scanner.nextInt();
             TheTablet[] TheTablet = new TheTablet[Object3];
             for (int i = 0; i < Object3; i++) {
@@ -76,24 +55,19 @@ public class Main {
                 TheTablet[i].create();
                 TheTablet[i].read();
             }
-            ShoppingCart cart = new ShoppingCart();
-            for (int i = 0; i < Object3; i++) {
-                cart.add(TheTablet[i]);
-            }
-            System.out.println("\nКол-во объектов: " + Products.CounterObject);
-            System.out.println("\n1.Оформить заказ\n2.Выход");
-            int choice3 = scanner.nextInt();
-            if (choice3 == 1) {
-                Orders orders = new Orders();
-                orders.offer(cart, user);
-                Thread.sleep(10);
-                orders.checkTime();
-                orders.show();
-                System.out.println("Проверка: " + cart.isExistsUUID(TheTablet[0].getUUID()));
-            } else {
-                return;
-            }
-        }
 
+            System.out.println("\nКол-во объектов: " + Products.CounterObject);
+            /**
+             * Пример работы функции update
+             */
+            System.out.println("Ручной ввод");
+            TheTablet[0].delete();
+            TheTablet[0].create();
+            TheTablet[0].read();
+        } else {
+            System.out.println("Invalid input");
+            return;
+        }
+        scanner.close();
     }
 }
